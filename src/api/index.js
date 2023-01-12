6f7b33daa1d9f3d36ab926fe4319d702cacd1a64
hugompereira2 <55194253+hugompereira2@users.noreply.github.com>
2023-01-12 17:10:34 -0300

    adjustment api hgbrasil error

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 export const getFinance = async () => {
   try {
-    const response = axios.get("api", {
+    const response = axios.get("/api", {
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
       }
     })
     return response;
@@ -16,10 +15,9 @@ export const getFinance = async () => {
 
 export const getHistory = async (currency = "USD") => {
   try {
-    const response = axios.get(`economia`+ `/${currency}-BRL/5`, {
+    const response = axios.get(`/economia`+ `/${currency}-BRL/5`, {
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
       }
     })
     return response;
